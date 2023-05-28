853cf89f072165c80c4e1e0d75028f7a9d878aa0
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2023-05-28 17:23:15 +0530

    files upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const counters=document.querySelectorAll('.counter');
+counters.forEach((counter)=> {
+    counter.innerHTML=0;
+    const updateCounter= () => {
+        const targetCount = +counter.getAttribute('data-target');
+        const startingCount=Number(counter.innerHTML);
+        const incr=targetCount /100;
+        if(startingCount< targetCount){
+            counter.innerHTML=`${Math.round(startingCount + incr)}`;
+            setTimeout(updateCounter,10);
+        } else{
+            counter.innerhtml=targetCount;
+        }
+    }
+    updateCounter();
+});
